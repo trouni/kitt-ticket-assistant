d4411c7f1015cf3fc728a07a4bde9b44ad7fd87b
Trouni Tiet <trouni@gmail.com>
2021-01-20 22:39:20 +0900

    Implemented ticket detection and notification

--- scripts/content.js
+++ scripts/content.js
@@ -1,54 +1,113 @@
-const synth = window.speechSynthesis;
-let ongoingTicket = false
-let ticketID = 0
-let muteAnnouncement = false
+const refreshRate = 1_000;
+const notificationRate = 30_000;
+const beep = new Audio(chrome.runtime.getURL("/assets/notification.mp3"));
+let ongoingTickets = true;
 
-const resetAssistant = _ => {
-  ongoingTicket = false
-  muteAnnouncement = false
-}
+const getVoices = (_) => {
+  return new Promise(function (resolve, reject) {
+    let synth = window.speechSynthesis;
 
-const initializeVoiceMessage = (studentName) => {
-  window.announcement = new SpeechSynthesisUtterance(`You have a ticket from ${studentName}.`);
-  window.announcement.voice = synth.getVoices()[49]
-}
+    let id = setInterval(() => {
+      if (synth.getVoices().length !== 0) {
+        resolve(synth.getVoices());
+        clearInterval(id);
+      }
+    }, 10);
+  });
+};
 
-const initUI = (studentName) => {
-  const assistantWrapper = document.createElement('div')
-  assistantWrapper.id = "kitt-assistant-modal"
+const initSpeech = (message) => {
+  let utterance = new SpeechSynthesisUtterance(message);
+  getVoices().then((voices) => {
+    utterance.voice = voices.find((el) => el.voiceURI === "Google US English");
+  });
+  utterance.volume = 0.3;
+  utterance.rate = 0.9;
+  return utterance;
+};
 
-  const heading = document.createElement('h3')
-  heading.innerText = `You have a new ticket from ${studentName}.`
+const initToggle = (_) => {
+  const assistantToggle = document.createElement("span");
 
-  const button = document.createElement('button')
-  button.classList.add("btn btn-warning btn-sm")
-  button.innerText = "OK (mute assistant)"
-  button.onclick = _ => {
-    muteAnnouncement = true
-    assistantWrapper.remove()
-  }
+  const toggleCheckbox = document.createElement("input");
+  toggleCheckbox.id = "kitt-assistant-checkbox";
+  toggleCheckbox.type = "checkbox";
+  toggleCheckbox.hidden = true;
 
-  assistantWrapper.insertAdjacentElement('afterbegin', button)
-  assistantWrapper.insertAdjacentElement('afterbegin', heading)
-  document.body.insertAdjacentElement('afterbegin', assistantWrapper)
-}
+  const toggleLabel = document.createElement("label");
+  toggleLabel.htmlFor = toggleCheckbox.id;
+  let disabledTooltip = "Ticket assistant is disabled. Click to enable";
+  let enabledTooltip =
+    "The ticket assistant will notify you when you have a ticket. Click to disable";
+  toggleLabel.dataset.originalTitle = disabledTooltip;
+  toggleLabel.dataset.toggle = "tooltip";
 
-window.setInterval(_ => {
-  var ticketDiv = document.querySelector(".ticket.is-mine")
-  if (ticketDiv) {
-    if (!ongoingTicket) {
-      initializeVoiceMessage(studentName)
-      ticketID += 1
-      initUI(studentName)
-      ongoingTicket = true
+  toggleCheckbox.onchange = (_) => {
+    if (toggleCheckbox.checked) {
+      toggleLabel.dataset.originalTitle = enabledTooltip;
+      window.assistant = startAssistant();
+    } else {
+      toggleLabel.dataset.originalTitle = disabledTooltip;
+      clearInterval(window.assistant);
     }
+  };
 
-    if (!muteAnnouncement) speechSynthesis.speak(window.announcement);
-  } else {
-    resetAssistant()
+  assistantToggle.insertAdjacentElement("beforeend", toggleCheckbox);
+  assistantToggle.insertAdjacentElement("beforeend", toggleLabel);
+
+  document
+    .querySelector('[data-target="navbar-menu.content"] > ul > li:first-child')
+    .insertAdjacentElement("afterbegin", assistantToggle);
+};
+
+document.addEventListener("DOMContentLoaded", initToggle);
+
+const displayModal = (message) => {
+  const assistantWrapper = document.createElement("div");
+  assistantWrapper.id = "kitt-assistant-modal";
+
+  const heading = document.createElement("h3");
+  heading.innerText = message;
+  assistantWrapper.insertAdjacentElement("beforeend", heading);
+  assistantWrapper.onclick = removeModal;
+
+  document.body.insertAdjacentElement("beforeend", assistantWrapper);
+};
+
+const removeModal = (_) => {
+  const assistantWrapper = document.getElementById("kitt-assistant-modal");
+  if (assistantWrapper) {
+    clearInterval(window.kittNotification);
+    window.speechSynthesis.cancel();
+    assistantWrapper.remove();
   }
-}, 5000)
+};
 
+const notify = (message) => {
+  beep.play();
+  let synthSpeech = initSpeech(message);
+  window.kittNotification = setInterval((_) => {
+    speechSynthesis.speak(synthSpeech);
+  }, notificationRate);
+};
 
-
-
+const startAssistant = (_) => {
+  return setInterval((_) => {
+    var ticketDiv = document.querySelector(".ticket.is-mine");
+    if (ticketDiv) {
+      if (!ongoingTickets) {
+        var studentName = ticketDiv.querySelector(".ticket .name").innerText;
+        var ticketNumber = ticketDiv
+          .querySelector(".ordinal-table .ordinal")
+          .innerText.toLowerCase();
+        var message = `You've been assigned ${studentName}'s ${ticketNumber}.`;
+        notify(message);
+        displayModal(message);
+        ongoingTickets = true;
+      }
+    } else {
+      removeModal();
+      ongoingTickets = false;
+    }
+  }, refreshRate);
+};
